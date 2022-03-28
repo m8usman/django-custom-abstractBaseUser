0f82d58addcfaf9045ba271c8ed8df6bf5047b60
mu8sman <u8sman@gmail.com>
2022-03-28 13:23:01 +0500

    psycopg2 installed

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d08o4659&lb6^j(091i^+0)1u+uiv!ro^!5)nbcpia7kvm&top'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Blog.wsgi --log-file -
+web: gunicorn DjangoBlog.wsgi --log-file -
